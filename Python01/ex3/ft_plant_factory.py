b8dfb7c859cb8ba2479457b0f8a8ff1cb8deada6
Blanditto <78723668+Marta-D4@users.noreply.github.com>
2026-01-24 15:58:59 +0100

    Update

--- Python01/ex3/ft_plant_factory.py
+++ Python01/ex3/ft_plant_factory.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+"""
+Represents a plant that can grow over time, with the attributes:
+    name (str): name of the plant, first letter capitalized.
+    height (int): height of the plant in centimeters.
+    days (int): age of the plant in days.
+def __init__ :
+    + Initializes a Plant instance.
+def grow:
+    + Increases by 1cm a plants height.
+def age:
+    + Increases by 1 day a plants age.
+def get_info:
+    + Returns a plants current information.
+"""
 class Plant():
     def __init__(self, name, height, days):
         self.name = name.capitalize()
@@ -15,7 +29,7 @@ class Plant():
     def get_info(self):
         return f"{self.name} ({self.height}cm, {self.days} days)"
 
-
+# List of Plant objects
 plants = [
     Plant("Rose", 25, 30),
     Plant("Oak", 200, 365),
@@ -26,9 +40,12 @@ plants = [
 
 print("=== Plant Factory Output ===")
 
+# Counts and show all created plants
 num_plants = 0
 for plant in plants:
     print(f"Created: {plant.get_info()}")
     num_plants += 1
 
+# Shows total number of plants created
 print(f"\nTotal plants created: {num_plants}")
+
