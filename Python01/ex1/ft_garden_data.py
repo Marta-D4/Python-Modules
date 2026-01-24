288a438e093fb0e5ecc2719fa0b7e2b0fb1bbe9f
Blanditto <78723668+Marta-D4@users.noreply.github.com>
2026-01-24 15:50:22 +0100

    Update

--- Python01/ex1/ft_garden_data.py
+++ Python01/ex1/ft_garden_data.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+"""
+Represents a plant with the attributes:
+    name (str): name of the plant, the first letter capitalized. 
+    height (int): height of the plant in centimeters.
+    age (int): age of the plant in days.
+The function inside initializes a new Plant instance.
+"""
 class Plant():
     def __init__(self, name, height, age):
         self.name = name.capitalize()
@@ -7,12 +14,15 @@ class Plant():
         self.age = age
 
 
+# List of Plant objects
 plants = [
     Plant("rose", 25, 30),
     Plant("sunflower", 80, 45),
     Plant("cactus", 15, 120)
 ]
 
+# Displays information of each plant registered
 print("=== Garden Plant Registry ===")
 for plant in plants:
     print(f"{plant.name}: {plant.height}cm, {plant.age} days old")
+
