0271f27a4c84c299ef26b86c1c25d63d83a00d08
Blanditto <78723668+Marta-D4@users.noreply.github.com>
2026-01-24 16:53:12 +0100

    Update

--- Python01/ex6/ft_garden_analytics.py
+++ Python01/ex6/ft_garden_analytics.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
+"""
+Represents a plant that can grow over time, with the attributes:
+    name (str): name of the plant, first letter capitalized.
+    height (int): height of the plant in centimeters.
+    
+def __init__ :
+    + Initializes a Plant instance.
+def grow:
+    + Increases by 1cm a plants height.
+def get_info:
+    + Returns a plants basic information.
+"""
 class Plant:
     def __init__(self, name, height):
         self.name = name.capitalize()
@@ -12,6 +24,15 @@ class Plant:
         return f"- {self.name}: {self.height}cm"
 
 
+"""
+Represents a flowering plant, subclass of Plant, with an additional attribute:
+    color (str): color of the flower.
+    
+def __init__:
+    + Initializes a FloweringPlant.
+def get_info: 
+    + Returns a flowering plants information, with its color.
+"""
 class FloweringPlant(Plant):
     def __init__(self, name, height, color):
         super().__init__(name, height)
@@ -21,6 +42,15 @@ class FloweringPlant(Plant):
         return f"{super().get_info()}, {self.color} flowers (blooming)"
 
 
+"""
+Represents a prize-winning flowering plant, subclass of FloweringPlant, 
+with an additional attribute:
+    prize_points (int): number of prize points aerned by the flower.
+def __init__:
+    + Initializes a PrizeFlower.
+def get_info: 
+    + Returns a prize flower plants information, with its prize points.
+"""
 class PrizeFlower(FloweringPlant):
     def __init__(self, name, height, color, prize_points):
         super().__init__(name, height, color)
@@ -30,10 +60,43 @@ class PrizeFlower(FloweringPlant):
         return f"{super().get_info()}, Prize points: {self.prize_points}"
 
 
+"""
+Manages multiple gardens, each with its own plants and statistics.
+    gardens (dic): dictionary that holds all created gardens.
+    total_gardens (int): total number of gardens managed.
+    
+def __init__:
+    + Initializes a GardenManager instance.
+def add_plant:
+    + Adds a plant to the garden and updates the garden stats.
+def grow_all:
+    + Helps all plants in the garden grow 1cm.
+def report:
+    + Prints a report of the gardens plants and stats.
+def validate height:
+    + Validates the height given.
+def create_garden_network:
+    + Returns a list of all the names of the gardens managed.
+def resume_scores:
+    + Prints the total number of gardens and its total points.
+"""
 class GardenManager:
     gardens = {}
     total_gardens = 0
 
+    """
+    Tracks stats for an specific garden, including plant types and growth.
+        total_growth (int): total growth in cm of all plants in the garden.
+        regular (int): number of regular plants in the garden.
+        flowering (int): number of flowering plants in the garden.
+        prize (int): number of prize-winnning plants in the garden.
+        points (int): total of prize points of a garden.
+
+    def __init__:
+        + Initializes the garden statistics.
+    def report:
+        + Prints a report of the garden stats.
+    """
     class GardenStats:
         def __init__(self):
             self.total_growth = 0
@@ -105,19 +168,26 @@ class GardenManager:
 
 print("=== Garden Management System Demo ===\n")
 
+# Creating gardens
 alice = GardenManager("Alice")
 bob = GardenManager("bob")
 
+# Adding plants
 alice.add_plant(Plant("Oak Tree", 100))
 alice.add_plant(FloweringPlant("Rose", 25, "red"))
 alice.add_plant(PrizeFlower("Sunflower", 50, "yellow", 10))
 
+# Growing and reporting the stats
 alice.grow_all()
 alice.report()
 
+# Updating the prize points of the gardens
 alice.stats.points += 208
 bob.stats.points += 92
 
+# Validating a height value
 GardenManager.validate_height(10)
 
+# Displays all gardens scores
 GardenManager.resume_scores()
+
